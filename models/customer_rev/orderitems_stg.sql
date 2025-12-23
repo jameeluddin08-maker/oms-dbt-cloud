23127dd1a5340b2f9eca24f4fa80b6c3fd4210b0
jameeluddin.08@gmail.com <jameeluddin.08@gmail.com>
2025-12-23 07:19:41 +0000

    changes for customer revenue report

---
+++ models/customer_rev/orderitems_stg.sql
@@ -0,0 +1,11 @@
+
+SELECT
+    OrderItemID,
+    OrderID,
+    ProductID,
+    Quantity,
+    UnitPrice,
+    Quantity * UnitPrice AS TotalPrice,
+    Updated_at
+FROM
+    {{ source('landing', 'orderitems') }}
